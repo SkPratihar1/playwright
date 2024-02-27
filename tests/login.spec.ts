5f0aae2bd0faa737931378d8c3194a2bb0a63cd1
Pratihar <pratihar@itobuz.com>
2024-02-27 15:13:32 +0530

    changes in workflow file

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,38 +1,38 @@
-// import { test, expect } from '@playwright/test';
-// import {LoginPage} from '../page/loginPage'
-// import {LogOutPage} from '../page/logoutPage'
+import { test, expect } from '@playwright/test';
+import {LoginPage} from '../page/loginPage'
+import {LogOutPage} from '../page/logoutPage'
 
-// test.beforeEach(async ({page}) => {
-//     await page.goto('/practice-test-login');
-// })
+test.beforeEach(async ({page}) => {
+    await page.goto('/practice-test-login');
+})
 
-// test.describe('Test log in', () => {
-//     const validCredentials = ['student', 'Password123'];
-//     const successfullyLoggedInURL = 'https://practicetestautomation.com/logged-in-successfully/';
+test.describe('Test log in', () => {
+    const validCredentials = ['student', 'Password123'];
+    const successfullyLoggedInURL = 'https://practicetestautomation.com/logged-in-successfully/';
     
 
-//     test('Test log in functionality with valid credentials', async ({page}) => {
-//         const loginPage = new LoginPage(page);
-//         await loginPage.logIn(validCredentials[0], validCredentials[1]);
-//         await loginPage.clickSubmitLoginButton();
+    test('Test log in functionality with valid credentials', async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.logIn(validCredentials[0], validCredentials[1]);
+        await loginPage.clickSubmitLoginButton();
 
-//         await expect(page).toHaveURL(successfullyLoggedInURL);
+        await expect(page).toHaveURL(successfullyLoggedInURL);
 
-//         const logOutButtonIsVisible = await loginPage.isLogOutButtonVisible();
-//         expect(logOutButtonIsVisible).toBe(true);
-//     })
+        const logOutButtonIsVisible = await loginPage.isLogOutButtonVisible();
+        expect(logOutButtonIsVisible).toBe(true);
+    })
     
-// })
-// test.describe('Test log out', () => {
+})
+test.describe('Test log out', () => {
     
-//     const successfullyLoggedInURL = 'https://practicetestautomation.com/logged-in-successfully/';
+    const successfullyLoggedInURL = 'https://practicetestautomation.com/logged-in-successfully/';
   
-//     test('Test log out', async ({page}) => {
-//         await page.goto(successfullyLoggedInURL)
-//         const logOutPage = new LogOutPage(page)
-//         await logOutPage.clickLogOut();
-//         await expect(page.locator("#login > h2")).toHaveText(/Test login/);
+    test('Test log out', async ({page}) => {
+        await page.goto(successfullyLoggedInURL)
+        const logOutPage = new LogOutPage(page)
+        await logOutPage.clickLogOut();
+        await expect(page.locator("#login > h2")).toHaveText(/Test login/);
         
         
-//     })
-// })
+    })
+})
